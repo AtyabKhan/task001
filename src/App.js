3befb057d8d82126f33d46d8f15c1b66c54ce71a
AtyabKhan <AtyabKhan@users.noreply.github.com>
2020-07-20 23:40:17 +0500

    app update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Chalo ab wapas Zameen py a jaao
+          Cyber Times
         </p>
         <a
           className="App-link"
