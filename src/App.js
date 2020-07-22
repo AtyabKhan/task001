52571d6ae3675cfd1834a42e120fccacf6b2898a
AtyabKhan <AtyabKhan@users.noreply.github.com>
2020-07-22 16:49:54 +0500

    Cyber Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Anotherfunction from './Anotherfunction'
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Anotherfunction testObj="this is just for testing"/>
-    </div>
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome To Cyber Times
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/channel/UCB3k84Q_u-Ehq93W9PiCFWw"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Youtube Channel
+        </a>
+      </header>
   );
 }
 
---
+++ src/Material.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Material (props){
+    return(
+       <div>
+           <h1>{props.materialName}</h1>
+           <hr/>
+       </div>
+        
+    );
+}
+export default Material;
